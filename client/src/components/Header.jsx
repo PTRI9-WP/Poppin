a51c8290f690afce6dd1765df9ae14a0ddd95e1f
Felix Leclerc Jr <felixLJR@icloud.com>
2023-01-06 23:49:35 -0500

    more styling completed ui bugs need to be fixed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,6 @@ import corkWhite from '../assets/images/corkWhite.png';
 
 const Header = () => {
   return (
-    <>
       <nav className='nav'>
         <div className='logoName'>
           <img src={corkWhite} alt='corks' className='navLogo' />
@@ -14,23 +13,6 @@ const Header = () => {
           <li>Register</li>
         </ul>
       </nav>
-      <div className='info'>
-        <ul className='promo'>
-          <li>
-            <h2>Hate Crowds?</h2>
-            <p>Find a great deal while being in a more intimate setting.</p>
-          </li>
-          <li>
-            <h2>Find The Right Party!</h2>
-            <p>With Poppin's proprietary crowd monitoring technology we'll help you find the livest crowd in town!</p>
-          </li>
-          <li>
-            <h2>Can't Get Enough People In The Door?</h2>
-            <p>Poppin will help people see your incetives and markeing efforts to get them in the door.</p>
-          </li>
-        </ul>
-      </div>
-    </>
   );
 };
 
--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -8,9 +8,31 @@ const LandingPage = () => {
     <>
       <div className='top'>
         <img src={bar} alt='bar' className='barPic' />
-        <Header className='header'/>
+        <Header className='header' />
       </div>
       {/* <Link to='/home'> temp link to dashboard </Link> */}
+      <div className='info'>
+        <ul className='promo'>
+          <li>
+            <h2>Hate Crowds?</h2>
+            <p className='b'>Find a great deal while being in a more intimate setting.</p>
+          </li>
+          <li>
+            <h2>Love Crowds?</h2>
+            <p>
+              With Poppin's proprietary crowd monitoring technology we'll help
+              you find the livest venue in town!
+            </p>
+          </li>
+          <li>
+            <h2>More People!</h2>
+            <p>
+              Poppin will help people see your incetives and markeing efforts to
+              get them in the door.
+            </p>
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
