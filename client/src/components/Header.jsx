d4d94b403c612308d180f9b774561e5a60c18a66
Felixljr <Felixljr@gmail.com>
2023-01-07 18:30:47 -0500

    Felxi njason/view creation css (#6)
    * landing page partially complete
    
    * more styling completed ui bugs need to be fixed
    
    * more styling and components created
    
    * some modals and other styling done
    
    * adding untracked changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import corkWhite from '../assets/images/corkWhite.png';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-      <nav className='nav'>
-        <div className='logoName'>
-          <img src={corkWhite} alt='corks' className='navLogo' />
-          <h1 className='title'>Poppin'</h1>
-        </div>
-        <ul className='menu'>
-          <li>Login</li>
-          <li>Register</li>
-        </ul>
-      </nav>
+    <nav className='nav'>
+      <div className='logoName'>
+        <img src={corkWhite} alt='corks' className='navLogo' />
+        <h1 className='title'>Poppin'</h1>
+      </div>
+      <ul className='menu'>
+        <Link to='/home'> temp link to dashboard </Link>
+        <li>Login</li>
+        <li>Register</li>
+      </ul>
+    </nav>
   );
 };
 
