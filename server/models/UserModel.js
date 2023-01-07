484be7aa3b7d42146fbe73bb8b7d4d914a4e4bbd
Jake R <jwr318@gmail.com>
2023-01-07 18:07:48 -0500

    created login and auth functionality

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -3,25 +3,26 @@ const db = require('../db');
 
 //By default, Sequelize automatically adds the primary key attribute id to every model when no primary key has been defined manually.
 const User = db.define('user', {
-  name: {
+  username: {
+    type: Sequelize.STRING,
+  },
+  password: {
     type: Sequelize.STRING,
   },
   email: {
     type: Sequelize.STRING,
     unique: true,
   },
-  password: {
-    type: Sequelize.STRING,
-  },
   location: {
     type: Sequelize.STRING,
   },
-  created: {
-    type: Sequelize.DATE,
-  },
-  updated: {
-    type: Sequelize.DATE,
-  },
+
+  // createdat: {
+  //   type: Sequelize.DATE,
+  // },
+  // updatedat: {
+  //   type: Sequelize.DATE,
+  // },
 });
 
 module.exports = User;
--- server/server.js
+++ server/server.js
@@ -3,31 +3,25 @@ const app = express();
 //this is how you pull in the env file
 require('dotenv').config();
 
-// const db = require('./db');
+const db = require('./db');
 const userRoutes = require('./routes/users');
 
-// db.authenticate()
-//   .then(() => console.log('Database connected...'))
-//   .catch((err) => console.log('ERROR', err));
+db.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch((err) => console.log('ERROR', err));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/users', userRoutes);
-
-app.get('/api', (_, res) => {
-  res.status(200).send('hello world');
-});
-
-//set up routes
 
 //catch all route handler
 app.use((_, res) => res.status(404).send('page not found'));
 
 //global error handler
-app.use((err, _, res, __) => {
+app.use((err, req, res) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
-  res.status(statusCode).json({
+  return res.status(statusCode).json({
     message: err.message ? err.message : 'An unknown error occured',
   });
 });
