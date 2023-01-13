25e934e901e4e7f72c2658f51c076027d08304e5
jaysenjonsin <jasoncjohnson5@yahoo.com>
2023-01-12 20:49:54 -0800

    add single card change functionality

--- client/src/components/BusinessCard.jsx
+++ client/src/components/BusinessCard.jsx
@@ -10,7 +10,7 @@ import {
 
 const BusinessCard = ({ businessCard, setShowCheckinModal }) => {
   const dispatch = useDispatch();
-  const [checkin, setCheckin] = useState(true);
+  // const [checkin, setCheckin] = useState(true);
 
   const { selectedBusiness } = useSelector((state) => state.businesses);
 
@@ -19,27 +19,6 @@ const BusinessCard = ({ businessCard, setShowCheckinModal }) => {
   //   dispatch(setSelectedBusiness(businessCard));
   //   console.log('SELECTED BUSINESS ==> ', selectedBusiness);
   // };
-
-  //code solution
-  //a code should exist on the database of the business << when /where is this code made
-  //when checkin button is pressed, retrieve code from business, console log code
-  //user takes consolelogged code and enters it into the input
-  //upon successful input, move code stored in code column into storedcodes column array
-  //upon succesfful checkout , access array from business table and acccess code column and delete the given user code from the array
-
-  //code solution felix
-  //all the codes are already stored in tobeusedcode column
-  //when a code is used by a user, move currentcode into storedcodes and pop a tobeusedcode and make it the currentcode
-
-  //Jake Solution
-  //create a state that tracks if a checkin button was pressed and a correct code was entered into the modal
-  //remove all checkin buttons
-  //the only way to checkout is to go to the checkin page and click checkout...you can still search for places, but no checkin button will be present
-
-  //Jason Solution
-  //CANNOT CLICK BUTTON IF THERE IS ALREADY A SELECTED BUSINESS
-  //
-
   const handleCheckin = (e) => {
     dispatch(setSelectedBusiness(businessCard));
     dispatch(
@@ -52,27 +31,27 @@ const BusinessCard = ({ businessCard, setShowCheckinModal }) => {
     dispatch(getAllBusinesses());
     console.log('CURRENT BUSINESS =>', selectedBusiness);
     console.log('SCORE =>', selectedBusiness?.poppinscore);
-    setCheckin(!checkin);
+    // setCheckin(!checkin);
     setShowCheckinModal(true);
   };
 
   return (
     <>
       {/* <div className = 'cardContainer'></div> */}
-      <div className="dashCard">
-        <div className="info1">
-          <img src={businessCard?.image} alt="img" />
+      <div className='dashCard'>
+        <div className='info1'>
+          <img src={businessCard?.image} alt='img' />
           {/* make sure to option chain (?), since this will be undefined until data is actually fetched. if no option chain, app will crash at run time instead of just temporarily returning undefined while data is fetching */}
           <div>{businessCard?.businessname}</div>
         </div>
-        <div className="info2">
+        <div className='info2'>
           <div>City: {businessCard?.location}</div>
           <div>Phone Number: {businessCard?.phonenumber}</div>
         </div>
-        <div className="info3">
+        <div className='info3'>
           {/* the question mark is needed as it AWAITS for the data to populate */}
           <div>Poppin Score {businessCard?.poppinscore}</div>
-          <div className="corkScore">
+          <div className='corkScore'>
             <GiChampagneCork
               color={businessCard?.poppinscore >= 20 ? '#2d3b46' : '#f1c9ba'}
             />
@@ -94,12 +73,12 @@ const BusinessCard = ({ businessCard, setShowCheckinModal }) => {
 
         {/* This need to change only when check in or out is confirmed */}
 
-        {!selectedBusiness ? (
-          <button className="checkinButton" onClick={handleCheckin}>
+        {selectedBusiness?.id === businessCard.id ? (
+          <button className='checkinButton' onClick={handleCheckin}>
             Check In
           </button>
         ) : (
-          <button className="attButton" onClick={handleCheckin}>
+          <button className='attButton' onClick={handleCheckin}>
             Check Out
           </button>
         )}
@@ -109,3 +88,23 @@ const BusinessCard = ({ businessCard, setShowCheckinModal }) => {
 };
 
 export default BusinessCard;
+
+//code solution
+//a code should exist on the database of the business << when /where is this code made
+//when checkin button is pressed, retrieve code from business, console log code
+//user takes consolelogged code and enters it into the input
+//upon successful input, move code stored in code column into storedcodes column array
+//upon succesfful checkout , access array from business table and acccess code column and delete the given user code from the array
+
+//code solution felix
+//all the codes are already stored in tobeusedcode column
+//when a code is used by a user, move currentcode into storedcodes and pop a tobeusedcode and make it the currentcode
+
+//Jake Solution
+//create a state that tracks if a checkin button was pressed and a correct code was entered into the modal
+//remove all checkin buttons
+//the only way to checkout is to go to the checkin page and click checkout...you can still search for places, but no checkin button will be present
+
+//Jason Solution
+//CANNOT CLICK BUTTON IF THERE IS ALREADY A SELECTED BUSINESS
+//
