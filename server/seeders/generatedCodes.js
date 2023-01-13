167238b63e7a5c275b43c221549d0ea426abe4af
jaysenjonsin <jasoncjohnson5@yahoo.com>
2023-01-13 02:56:28 -0800

    add more seeders

--- server/seeders/generatedCodes.js
+++ server/seeders/generatedCodes.js
@@ -1,4 +1,4 @@
-//seeders: data used for initial population in a db. Used for testing, ex. we wouldn't use this in a working app, but I just made it to see if code works
+//seeders: data used for initial population in a db. Used for testing, ex. we wouldn't use this in a working app, but I just made it to see if code works. It could also be randomly generated data
 const generatedCodes = [
   'jake',
   'tim',
