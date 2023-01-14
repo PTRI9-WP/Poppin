4921ac058fe8ac13b492e0fd731dd519ebb17267
Felix Leclerc Jr <felixLJR@icloud.com>
2023-01-14 14:29:13 -0500

    ui updates complete

--- client/src/components/Alert.jsx
+++ client/src/components/Alert.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 const Alert = () => {
 
   return (
-    <div className='w-[100%] h-fit bottom-0 bg-red-400 absolute z-50'>
+    <div className='w-[100%] h-fit bottom-0 bg-red-400 absolute z-50 animate-bounce'>
       <h2 className='modalTitle'>Invalid Code</h2>
     </div>
   );
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -22,7 +22,7 @@ const Dashboard = () => {
   const [searchBox, setSearchBox] = useState(null);
   const [location, setLocation] = useState(null);
   const [markers, setMarkers] = useState(null);
-
+  const [showCards, setShowCards ] = useState(false);
   const { user } = useSelector((state) => state.auth);
   //show modal for entering checkin code
   const [showCheckinModal, setShowCheckinModal] = useState(false);
@@ -91,6 +91,7 @@ const Dashboard = () => {
         />
       );
     });
+    
     return markersArr;
   };
 
@@ -102,6 +103,7 @@ const Dashboard = () => {
     bounds.union(places[0].geometry.viewport);
     map.fitBounds(bounds);
     setMarkers(await createMarkers());
+    setShowCards(true);
   };
 
   //set the reference object to the searchbox state upon the searchbox component rendering
@@ -162,22 +164,24 @@ const Dashboard = () => {
           </div>
           {/* End User Form Section */}
           {/* Map section */}
-          <div className="map">
-              <GoogleMap
-                mapContainerStyle={containerStyle}
-                center={location}
-                zoom={10}
-                onLoad={onMapLoad}
-              >
-                {markers}
-              </GoogleMap>
+          <div className='map'>
+            <GoogleMap
+              mapContainerStyle={containerStyle}
+              center={location}
+              zoom={10}
+              onLoad={onMapLoad}
+            >
+              {markers}
+            </GoogleMap>
           </div>
           {/* End Map section */}
           {/* pic - <address / phone > <poppin score/ incentive>  <checkin>*/}
-          <CardContainer
-            setShowCheckinModal={setShowCheckinModal}
-            showCheckinModal={showCheckinModal}
-          />
+          { showCards ?
+            <CardContainer
+              setShowCheckinModal={setShowCheckinModal}
+              showCheckinModal={showCheckinModal}
+            /> : null
+          }
         </main>
       </div>
       {showCheckinModal ? (
